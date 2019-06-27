8f04fdd84a2cd8ae80e63b418a6540146d7c9e67
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-06-27 20:59:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function driverNamesWithRevenueOver(driversArr, num) {
 
 function exactMatch(arr, matchObj){
   return arr.filter(function(driver){
-    return driver.
+    return driver.matchObj.keys[0] === 
   });
 };
